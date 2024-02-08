64b80212edff8661c383941744727800d82d85f0
Kristin Evans <kristinlashun@gmail.com>
2024-02-07 19:32:02 -0800

    add SatData class to process SAT JSON data

--- SatData.py
+++ SatData.py
@@ -7,7 +7,7 @@ import json
 
 class SatData:
     def __init__(self, json_file='sat.json'):
-    
+        """Initialize the SatData object, reading the SAT data from a JSON file."""
         self.__data = self.__read_json(json_file)
 
     def __read_json(self, file_path):
@@ -17,30 +17,29 @@ class SatData:
 
     def __sanitize_data(self, data):
         """Enclose data in quotes if it contains a comma."""
-        return f'"{data}"' if ',' in data else data
+        return f'"{data}"' if ',' in data or '"' in data else data
 
     def save_as_csv(self, dbns):
-        
+        """Save data as CSV for the provided DBNs."""
         with open('output.csv', 'w') as file:
             # Write column headers
             file.write('DBN,School Name,Number of Test Takers,Critical Reading Mean,Mathematics Mean,Writing Mean\n')
             
-            # Sort the data by DBN
-            sorted_data = sorted(
-                (entry for entry in self.__data if entry['dbn'] in dbns),
-                key=lambda x: x['dbn']
-            )
+            # Filter and sort the data by DBN
+            filtered_data = [entry for entry in self.__data if entry['dbn'] in dbns]
+            sorted_data = sorted(filtered_data, key=lambda x: x['dbn'])
 
             # Write the sorted data to the CSV file
             for entry in sorted_data:
                 dbn = entry['dbn']
                 school_name = self.__sanitize_data(entry['school_name'])
                 num_of_test_takers = entry['num_of_test_takers']
-                critical_reading_mean = entry['critical_reading_mean']
-                math_mean = entry['math_mean']
-                writing_mean = entry['writing_mean']
+                critical_reading_mean = entry['sat_critical_reading_avg_score']
+                math_mean = entry['sat_math_avg_score']
+                writing_mean = entry['sat_writing_avg_score']
 
                 line = f'{dbn},{school_name},{num_of_test_takers},{critical_reading_mean},{math_mean},{writing_mean}\n'
                 file.write(line)
+
 
 
